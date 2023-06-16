b39c7d7eb69d7df78890e14aeb13a6bf876462ce
EmilieLukaszewskiRestart <127516495+EmilieLukaszewskiRestart@users.noreply.github.com>
2023-06-16 21:37:43 +0200

    Delete renduaxe directory

--- renduaxe/getONE.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'PHP/databasebbhub.php';
-$nom = $database->prepare("SELECT id, contenu, tag, date FROM posts ORDER BY id DESC LIMIT 10");
-$nom->execute();
-$resultat = $nom->fetch(PDO::FETCH_ASSOC);
-
-echo json_encode($resultat);
-?>
